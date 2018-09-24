b965c6f1ceb86340d8cf88fc4f8b79548c03dbeb
Rhytah <ritanamono@gmail.com>
2018-09-24 12:33:02 +0300

    show dog name and age

--- pets_class.py
+++ pets_class.py
@@ -14,11 +14,19 @@ class Pets:
     fletcher=Dog(name='Fletcher',kind='mammal',age=7)
     larry=Dog(name='Larry',kind='mammal',age=9)
 
-
     my_pets=[tom,fletcher,larry]
-    print("I have three dogs")
-    
-    print ("And they're all mammals, of course.")
 
+    # print("I have three dogs")
+    # print ("{} is {}.".format(tom.name, tom.age))
+    # print("{} is {}.".format(fletcher.name, fletcher.age))
+    # print("{} is {}.".format(larry.name, larry.age))
+    # print ("And they're all mammals, of course.")
+    print ("I have 3 dogs.")
+
+    for pet in my_pets:
+        print(pet.name,"is",pet.age)
+        
+    print ("And they're all mammals, of course.")
+    
     
     

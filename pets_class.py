037523b4a56c274ed7b133bebdeea0fb8435e4a9
Rhytah <ritanamono@gmail.com>
2018-09-24 15:11:03 +0300

    feed my dogs

--- pets_class.py
+++ pets_class.py
@@ -3,21 +3,42 @@ class Dog:
         self.name=name
         self.kind=kind
         self.age=age
+        # self.is_hungry=is_hungry
+
+    def eat(self,is_hungry):
+        if is_hungry==True:
+            return False
         
-    def eat(self):
-        return False 
+        
     
-               
-
 class Pets:
-    tom=Dog(name='Tom',kind='mammal',age=6)
-    fletcher=Dog(name='Fletcher',kind='mammal',age=7)
-    larry=Dog(name='Larry',kind='mammal',age=9)
-
+    tom=Dog(name='Tom',kind='mammal',age=6, is_hungry=True)
+    fletcher=Dog(name='Fletcher',kind='mammal',age=7,is_hungry=True)
+    larry=Dog(name='Larry',kind='mammal',age=9,is_hungry=True)
 
     my_pets=[tom,fletcher,larry]
-    print("I have three dogs")
+    
+    # print("I have three dogs")
+    # print ("{} is {}.".format(tom.name, tom.age))
+    # print("{} is {}.".format(fletcher.name, fletcher.age))
+    # print("{} is {}.".format(larry.name, larry.age))
+    # print ("And they're all mammals, of course.")
+    print ("I have 3 dogs.")
+    is_hungry=True
+    
+
+    for pet in my_pets:    
+        print(pet.name,"is",pet.age)
+        pet.eat(is_hungry)
+        # print("my dog is not hungry")
+        # print(pet.kind)
     print ("And they're all mammals, of course.")
+    print("My dogs are not hungry")
+        
+      
+    
+
+            
 
     
     
